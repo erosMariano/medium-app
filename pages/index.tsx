db979194db07e084942c81a2ec075b1216bb1216
erosMariano <erosmariano1@gmail.com>
2022-01-24 20:52:14 -0300

    fetch in posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typiings";
 
-export default function Home() {
+interface Props {
+	posts: [Post]
+}
+
+export default function Home({posts}: Props) {
+	
+	console.log(posts)
 	return (
 		<div className="max-w-7xl mx-auto">
 			<Head>
@@ -33,7 +41,33 @@ export default function Home() {
 				</div>
 			</div>
 
-      {/* {Posts} */}
+			{/* {Posts} */}
 		</div>
 	);
 }
+
+export const getServerSideProps = async () => {
+	const query = `
+	
+	*[_type == "post"]{
+		_id,
+		 title,
+		 slug,
+		 author -> {
+			name, 
+			image
+	   	},
+		   description,
+		   mainImage,
+		   slug
+	   }
+	`;
+
+	const posts = await sanityClient.fetch(query);
+
+	return{
+		props: {
+			posts, 
+		}
+	}
+};
