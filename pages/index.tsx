c025e01b5525f1cb50434b6c96745549b22e230f
erosMariano <erosmariano1@gmail.com>
2022-01-25 14:16:43 -0300

    implementing the Post section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
 import { sanityClient, urlFor } from "../sanity";
 import { Post } from "../typiings";
 
 interface Props {
-	posts: [Post]
+	posts: [Post];
 }
 
-export default function Home({posts}: Props) {
-	
-	console.log(posts)
+export default function Home({ posts }: Props) {
+	console.log(posts);
 	return (
 		<div className="max-w-7xl mx-auto">
 			<Head>
@@ -42,6 +42,44 @@ export default function Home({posts}: Props) {
 			</div>
 
 			{/* {Posts} */}
+
+			<div>
+				{posts.map((post) => {
+					return (
+						<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p -6">
+							<Link
+								key={post._id}
+								href={`/posts/${post.slug.current}`}
+							>
+								<div className="border rounded-lg group cursor-ponter overflow-hidden">
+									<img
+										className="h-60 w-full object-cover group-hover:scale-105 transition-transfrom duration-200 ease-in-out "
+										src={urlFor(post.mainImage).url()!}
+										alt=""
+									/>
+									<div className="flex justify-between p-5 bg-white">
+										<div>
+											<p className="text-lg font-bold">{post.title}</p>
+											<p className="text-xs">
+												{post.description} by{" "}<br/>
+												{post.author.name}{" "}
+											</p>
+										</div>
+
+										<img
+											className="h-12 w-12 rounded-full"
+											src={
+												urlFor(post.author.image).url()!
+											}
+											alt=""
+										/>
+									</div>
+								</div>
+							</Link>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
@@ -65,9 +103,9 @@ export const getServerSideProps = async () => {
 
 	const posts = await sanityClient.fetch(query);
 
-	return{
+	return {
 		props: {
-			posts, 
-		}
-	}
+			posts,
+		},
+	};
 };
