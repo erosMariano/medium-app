0b20cd1dce31945129333dd0c50af16fcd216e26
erosMariano <erosmariano1@gmail.com>
2022-01-25 17:45:43 -0300

    Building the post page

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,7 +4,7 @@ function Header() {
 	return (
 		<header className="flex justify-between p-5 max-w-7xl mx-auto">
 			<div className="flex items-center space-x-5">
-				<Link href="/post">
+				<Link href="/">
 					<img
 						className="w-44 object-contain cursor-pointer"
 						src="https://links.papareact.com/yvf"
--- pages/index.tsx
+++ pages/index.tsx
@@ -42,39 +42,38 @@ export default function Home({ posts }: Props) {
 
 			{/* {Posts} */}
 
-			<div>
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
 				{posts.map((post) => {
 					return (
-						<div key={post._id} className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p -6">
-							<Link
-								href={`/posts/${post.slug.current}`}
-							>
-								<div className="border rounded-lg group cursor-ponter overflow-hidden">
+						<Link
+							key={post._id}
+							href={`/posts/${post.slug.current}`}
+						>
+							<div className="border rounded-lg group cursor-pointer overflow-hidden">
+								<img
+									className="h-60 w-full object-cover group-hover:scale-105 transition-transfrom duration-200 ease-in-out "
+									src={urlFor(post.mainImage).url()!}
+									alt=""
+								/>
+								<div className="flex justify-between p-5 bg-white">
+									<div>
+										<p className="text-lg font-bold">
+											{post.title}
+										</p>
+										<p className="text-xs">
+											{post.description} by <br />
+											{post.author.name}{" "}
+										</p>
+									</div>
+
 									<img
-										className="h-60 w-full object-cover group-hover:scale-105 transition-transfrom duration-200 ease-in-out "
-										src={urlFor(post.mainImage).url()!}
+										className="h-12 w-12 rounded-full"
+										src={urlFor(post.author.image).url()!}
 										alt=""
 									/>
-									<div className="flex justify-between p-5 bg-white">
-										<div>
-											<p className="text-lg font-bold">{post.title}</p>
-											<p className="text-xs">
-												{post.description} by{" "}<br/>
-												{post.author.name}{" "}
-											</p>
-										</div>
-
-										<img
-											className="h-12 w-12 rounded-full"
-											src={
-												urlFor(post.author.image).url()!
-											}
-											alt=""
-										/>
-									</div>
 								</div>
-							</Link>
-						</div>
+							</div>
+						</Link>
 					);
 				})}
 			</div>
